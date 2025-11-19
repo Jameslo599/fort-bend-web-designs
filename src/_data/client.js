7c1f7ffc93fd881ec7b49f7e1341a27d7681b43c
James Lo <73731156+Jameslo599@users.noreply.github.com>
2025-11-19 03:52:31 -0600

    added features

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,25 @@
 module.exports = {
-  name: 'L&L Contracting',
-  nameCaps: 'L&L CONTRACTING',
-  email: 'toptiercontractingco@gmail.com',
-  phoneForTel: '+14693839184',
-  phoneFormatted: '(469) 383-9184',
+  name: 'Fort Bend Web Designs',
+  nameCaps: 'FORT BEND WEB DESIGNS',
+  email: 'fortbendwebdesigns@gmail.com',
+  phoneForTel: '+12149010531',
+  phoneFormatted: '(214) 901-0531',
   address: {
-    lineOne: '5140 Old Buena Vista Rd.',
+    lineOne: 'XXXX',
     lineTwo: 'XXXX',
-    city: 'Waxahachie',
-    state: 'TX',
-    zip: '75167',
-    country: 'USA',
-    mapLink: '#',
+    city: 'XXXX',
+    state: 'XXXX',
+    zip: 'XXXX',
+    country: 'United States of America',
+    mapLink:
+      'google.com/maps/place/Fort+Bend+Web+Designs/data=!4m2!3m1!1s0x0:0x375cbb3ba4f74b2b?sa=X&ved=1t:2428&hl=en&ictx=111',
   },
   socials: {
-    facebook: '#',
+    facebook:
+      'https://www.facebook.com/people/Fort-Bend-Web-Designs/61582613360630/#',
   },
   //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-  domain: 'https://www.toproof.netlify.app',
+  domain: 'https://www.fortbendwebdesigns.com',
   // Passing the isProduction variable for use in HTML templates
   isProduction: process.env.ELEVENTY_ENV === 'PROD',
 };
